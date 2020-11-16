8a66437c4bf6bb0a168c17db0f113e3d09712d91
ewilso <17hughwilson@gmail.com>
2020-11-16 23:05:57 +0000

    latest

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def main():
    return render_template('index.html')
 
+@app.route('/porturia')
+def porturia():
+   return render_template('PS.html')
+
 if __name__ == '__main__':
    app.run()
